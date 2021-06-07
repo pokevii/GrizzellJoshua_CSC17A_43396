6220bfdbf8df2e82db098e772b0446666fc78b66
pokevii <jgrizzell2002@gmail.com>
2021-06-07 15:18:04 -0700

    Signed-off-by: pokevii <jgrizzell2002@gmail.com>

---
+++ Hmwk/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Joshua Grizzell
+ *
+ * Created on February 23, 2021, 2:52 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    cout << "Hello, world!";
+    
+    return 0;
+}
+
---
+++ Hmwk/HelloWorld/README.md
@@ -0,0 +1,2 @@
+# GrizzellJoshua_CSC17A_43396
+the repository for all of my code made for RCC's CSC17A class.
